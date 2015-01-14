1fc5102effe41e5df840cf50fd8ff04826515674
Jonghyun Bae <bnbbkr@gmail.com>
2015-01-14 12:33:47 +0900

    Update server.R

--- inst/examples/example_offload/server.R
+++ inst/examples/example_offload/server.R
@@ -1,3 +1,4 @@
+
 AlldataArr <- scan("./www/data.js", what="")
 firstag <- FALSE
 changeJSON <- FALSE
@@ -49,63 +50,12 @@ repeat {
 
 shinyServer(function(input, output) {
 
-	output$loessArray1 <- reactive({ 
-		if (length(input$subDiamonds) != 0) { 
-			if (length(input$subDiamonds) > 1) { 
-				subDiamonds <- .subDiamonds[!input$subDiamonds, ]
-				{
-    obj <- loess(carat ~ price, data = subDiamonds)
-    xRange <- range(subDiamonds$price)
-    simArray <- data.frame(price = seq(xRange[1], xRange[2], length.out = 132))
-    simArray$carat <- predict(obj, newdata = simArray)
-    return(simArray)
-}
-			} else { 
-				output <- list(-1, -1) 
-				return (output) 
-			}
-		}
-	})
-	output$lmArray1 <- reactive({ 
-		if (length(input$subDiamonds) != 0) { 
-			if (length(input$subDiamonds) > 1) { 
-				subDiamonds <- .subDiamonds[!input$subDiamonds, ]
-				{
-    obj <- lm(carat ~ price, data = subDiamonds)
-    xRange <- range(subDiamonds$price)
-    simArray <- data.frame(price = seq(xRange[1], xRange[2], length.out = 132))
-    simArray$carat <- predict(obj, newdata = simArray)
-    return(simArray)
-}
-			} else { 
-				output <- list(-1, -1) 
-				return (output) 
-			}
-		}
-	})
-	output$loessArray2 <- reactive({ 
+	output$loessArray <- reactive({ 
 		if (length(input$Theoph) != 0) { 
 			if (length(input$Theoph) > 1) { 
 				Theoph <- .Theoph[!input$Theoph, ]
 				{
     obj <- loess(conc ~ Time, data = Theoph)
-    xRange <- range(Theoph$Time)
-    simArray <- data.frame(Time = seq(xRange[1], xRange[2], length.out = 132))
-    simArray$conc <- predict(obj, newdata = simArray)
-    return(simArray)
-}
-			} else { 
-				output <- list(-1, -1) 
-				return (output) 
-			}
-		}
-	})
-	output$lmArray2 <- reactive({ 
-		if (length(input$Theoph) != 0) { 
-			if (length(input$Theoph) > 1) { 
-				Theoph <- .Theoph[!input$Theoph, ]
-				{
-    obj <- lm(conc ~ Time, data = Theoph)
     xRange <- range(Theoph$Time)
     simArray <- data.frame(Time = seq(xRange[1], xRange[2], length.out = 132))
     simArray$conc <- predict(obj, newdata = simArray)
